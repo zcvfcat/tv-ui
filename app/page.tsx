c3c078519a86b30402b1f1948759e264c9e415cb
zcvfcat <zcvfcat@gmail.com>
2023-09-07 21:29:52 +0900

    feat (dot-env)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { env } from "process"
+
 export default function Home() {
   return (
     <main>
-      <header>헤더</header>
+      <header>헤더 환경 : {env.TEST}</header>
       <nav>내비바</nav>
       <section>본문</section>
       <aside>광고바</aside>
